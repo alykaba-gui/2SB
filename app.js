fe5eb8768dd076860f0d2f4afb0a1c3b68c68332
unknown <kabaaly>
2025-09-30 01:41:58 +0300

    rail27

--- app.js
+++ app.js
@@ -29,12 +29,15 @@ try {
 }
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || "smtp.gmail.com",
-  port: process.env.SMTP_PORT || 465,
+  port: process.env.SMTP_PORT || 587,
   secure: false, // true pour 465, false pour autres ports
   service: "gmail",
   auth: {
     user: "alykaba3006@gmail.com",
     pass: "mfkewfzzjppfrssr",
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
   connectionTimeout: 30000,
   socketTimeout: 30000,
